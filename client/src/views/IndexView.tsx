225fc48e17d624f6c49038e8aea109c075c57103
Richard Ng <richardcrng@gmail.com>
2023-01-04 20:06:02 +0000

    Add Join Game button

--- client/src/views/IndexView.tsx
+++ client/src/views/IndexView.tsx
@@ -1,16 +1,36 @@
 import IntroFrame from "../ui/molecules/IntroFrame";
+import { generateRandomGameId } from "../utils/data-utils";
 
 interface Props {
   onHostNew(): void;
+  onNavigateToJoinGame(gameId: string): void;
+  onNavigateToHostGame(): void;
 }
 
-export default function IndexView({ onHostNew }: Props): JSX.Element {
+export default function IndexView({ onHostNew, onNavigateToHostGame, onNavigateToJoinGame }: Props): JSX.Element {
+
+  const handleTryJoinGame = () => {
+    const gameId = window.prompt(`Please enter the 5 character game ID provided by your host, e.g. ${generateRandomGameId()}`);
+
+    if (!gameId) {
+      const result = window.confirm("It looks like you didn't provide a game ID - do you want to try hosting a new game?");
+      result && onNavigateToHostGame()
+    } else {
+      onNavigateToJoinGame(gameId.toUpperCase())
+    }
+  }
+  
   return (
     <IntroFrame>
       <p className="text-xl">{"[[ADD GAME DESCRIPTION]]"}</p>
-      <button className="btn btn-block btn-xl" onClick={onHostNew}>
-        Host new
-      </button>
+      <div className="w-full flex flex-col gap-y-2">
+        <button className="btn btn-primary btn-block btn-xl" onClick={handleTryJoinGame}>
+          Join game
+        </button>
+        <button className="btn btn-secondary btn-block btn-xl" onClick={onHostNew}>
+          Host new
+        </button>
+      </div>
     </IntroFrame>
   );
 }
